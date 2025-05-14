7e48e438bc3e44ead5fcf8eebef1923b77dd741e
Mykhaylo Dyachenko <mihaeldyachenko@gmail.com>
2025-05-13 19:40:31 -0500

    if_elif_else done

--- Basics/if_elif_else/sprint_05/15.py
+++ Basics/if_elif_else/sprint_05/15.py
@@ -2,7 +2,10 @@
 # Напишите функцию f15, которая принимает логин и пароль. Если логин не пустая строка и пароль больше 5 символов возвращает True. В остальных случаях - False.
 
 def f15(login, password):
-    pass
+    if login != '' and len(password) > 5:
+        return True
+    else:
+        return False
     
 
 

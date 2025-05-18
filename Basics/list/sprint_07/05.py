535993c2ca7b9cc275b033f2f198c8120ba09d57
Mykhaylo Dyachenko <mihaeldyachenko@gmail.com>
2025-05-17 19:16:24 -0500

    learning list in Python

--- Basics/list/sprint_07/05.py
+++ Basics/list/sprint_07/05.py
@@ -1,13 +1,15 @@
 # itgid.info - python 2023
 
-# Напишите функцию, которая получает list как аргумент и возвращает True, если элемент есть в list, и False если нет.
+# Напишите функцию, которая получает list как аргумент и возвращает True, если элемент есть в list,
+# и False если нет.
 
 # write your code under this line
 
 m5 = [999, 888, 777, 666, 555, 444, 333, 222, 111]
 
-def f05 (ct) :
-    pass
+def f05 (lst, element) :
+    return element in lst
+    
 
-result = f05(m5)
+result = f05(m5, 775)
 print(result)
--- Basics/list/sprint_07/06.py
+++ Basics/list/sprint_07/06.py
@@ -1,13 +1,18 @@
 # itgid.info - python 2023
 
-# Напишите функцию, которая получает list как аргумент и возвращает 1, если элемент есть в list, и 0 если нет.
+# Напишите функцию, которая получает list как аргумент и возвращает 1,
+#  если элемент есть в list, и 0 если нет.
 
 # write your code under this line
 
 m6 = [999, 888, 777, 666, 555, 444, 333, 222, 111]
 
-def f06 (ct) :
-    pass
+def f06 (lst, element) :
+    if element in lst:
+        return 1
+    else:
+        return 0
+    
 
-result = f06(m6)
+result = f06(m6, 778)
 print(result)

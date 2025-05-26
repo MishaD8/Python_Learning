e5b4bc2926cf8cb290c188e26d690f245b2d0855
Mykhaylo Dyachenko <mihaeldyachenko@gmail.com>
2025-05-25 20:43:34 -0500

    starting learn tuples

---
+++ Basics/tuples/code/check_difference.py
@@ -0,0 +1,8 @@
+
+foo = ['hello', 1, 2, 'hello']
+bar = ('hi', 33, 44, 'hi')
+
+print(foo)
+print(bar)
+
+print('\n >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> \n')
--- Basics/tuples/sprint_09/03.py
+++ Basics/tuples/sprint_09/03.py
@@ -4,7 +4,11 @@
 # Напишите функцию f03, получает list как аргумент, создает на его основе tuple и возвращает его.
 
 def f03(m) :
-    pass
+    tpl = tuple(m)
+
+    return tpl
+
+
 
 
 lst = ["apple", "banana", "cherry"]

0ac7a2f69d9baad842eb647baffad40f4d88b095
Mykhaylo Dyachenko <mihaeldyachenko@gmail.com>
2025-05-28 07:08:54 -0500

    tuples done

--- Basics/tuples/sprint_09/12.py
+++ Basics/tuples/sprint_09/12.py
@@ -4,7 +4,11 @@
 # Напишите функцию f12, которая получает tuple и возвращает сумму его элементов.
 
 def f12(t1) :
-    pass
+    total = 0
+    for item in t1:
+        total += item
+        
+    return total
 
 
 tpl = (100, 105, 110)

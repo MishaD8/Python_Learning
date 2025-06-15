b6aeff49e396c03c97527233f8beb2343e9da518
Mykhaylo Dyachenko <mihaeldyachenko@gmail.com>
2025-06-15 13:30:57 -0500

    working with a sockets

---
+++ Sockets/client.py
@@ -0,0 +1,11 @@
+import socket
+
+# socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = '127.0.0.1'
+port = 9001
+
+# connect
+
+s.connect((host, port))
---
+++ Sockets/server.py
@@ -0,0 +1,23 @@
+
+import socket
+
+# socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print(type(s))
+
+host = '127.0.0.1'
+port = 9001
+
+# bind
+
+s.bind((host, port)) # should be over 1024
+
+# listen
+
+s.listen() # now waiting
+
+# accept
+
+conn, addr = s.accept() # not do this until someone is trying to connect a client
+print(f'Connection receive from {addr}')
